51de2d4b8a6cb9f18365296ae968cff585822426
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2010-10-21 02:10:01 +0000

    Class for DesktopName pseudo-encoding
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@299 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

---
+++ Source/DesktopNameEncodingReader.m
@@ -0,0 +1,50 @@
+/* DesktopNameEncodingReader.m
+ * Copyright (C) 2010 Dustin Cartwright
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#import "DesktopNameEncodingReader.h"
+#import "FrameBufferUpdateReader.h"
+#import "RFBConnection.h"
+#import "RFBStringReader.h"
+
+/* Pseudo-encoding to support renaming the desktop. Never actually tested,
+ * because I couldn't find a server which supports this pseudo-encoding. */
+@implementation DesktopNameEncodingReader
+
+- (void)dealloc
+{
+    [nameReader release];
+    [super dealloc];
+}
+
+- (void)readEncoding
+{
+    if (!nameReader)
+        nameReader = [[RFBStringReader alloc] initTarget:self
+                                                  action:@selector(nameRead:)
+                                              connection:connection];
+    [nameReader readString];
+}
+
+- (void)nameRead:(NSString *)newName
+{
+    [connection setDisplayName:newName];
+    [updater didRect:self];
+}
+
+@end

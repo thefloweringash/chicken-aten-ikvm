5a2e541597736a009b8e844e7b019a09e55d6f65
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2010-10-21 03:11:43 +0000

    Support for: DesktopName, RichCursor, PointerPos, LastRect, DesktopSize pseudo-encodings. Only marks invalid rects at the end of an update.
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@315 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

--- Source/FrameBufferUpdateReader.m
+++ Source/FrameBufferUpdateReader.m
@@ -20,6 +20,8 @@
 
 #import "FrameBufferUpdateReader.h"
 #import "ByteBlockReader.h"
+#import "CursorPseudoEncodingReader.h"
+#import "DesktopNameEncodingReader.h"
 #import "RawEncodingReader.h"
 #import "CopyRectangleEncodingReader.h"
 #import "CoRREEncodingReader.h"
@@ -27,6 +29,7 @@
 #import "PrefController.h"
 #import "RFBConnection.h"
 #import "RFBConnectionManager.h"
+#import "RFBProtocol.h"
 #import "RREEncodingReader.h"
 #import "TightEncodingReader.h"
 #import "ZlibEncodingReader.h"
@@ -37,29 +40,33 @@
 
 @implementation FrameBufferUpdateReader
 
-- (id)initTarget:(id)aTarget action:(SEL)anAction
+- (id)initWithProtocol: (RFBProtocol *)aProtocol connection: (RFBConnection *)aConnection;
 {
-    if (self = [super initTarget:aTarget action:anAction]) {
-		PrefController *prefs = [PrefController sharedController];
-		int pst = [prefs PS_THRESHOLD];
-		int mpr = [prefs PS_MAXRECTS];
+    if (self = [super init]) {
+		connection = aConnection;
+        protocol = aProtocol;
+        bytesPerPixel = 0;
 
 		headerReader = [[ByteBlockReader alloc] initTarget:self action:@selector(setHeader:) size:3];
-		rawEncodingReader = [[RawEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		copyRectangleEncodingReader = [[CopyRectangleEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		rreEncodingReader = [[RREEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		coRreEncodingReader = [[CoRREEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		hextileEncodingReader = [[HextileEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		tightEncodingReader = [[TightEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		zlibEncodingReader = [[ZlibEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		zrleEncodingReader = [[ZRLEEncodingReader alloc] initTarget:self action:@selector(didRect:)];
-		zlibHexEncodingReader = [[ZlibHexEncodingReader alloc] initTarget:self action:@selector(didRect:)];
 		rectHeaderReader = [[ByteBlockReader alloc] initTarget:self action:@selector(setRect:) size:12];
-		connection = [target topTarget];
-		[rreEncodingReader setPSThreshold:pst];
-		[coRreEncodingReader setPSThreshold:pst];
-		[rreEncodingReader setMaximumPSRectangles:mpr];
-		[coRreEncodingReader setMaximumPSRectangles:mpr];
+
+        rawEncodingReader = [[RawEncodingReader alloc] initWithUpdater: self connection: connection];
+        copyRectangleEncodingReader = [[CopyRectangleEncodingReader alloc] initWithUpdater: self connection: connection];
+        rreEncodingReader = [[RREEncodingReader alloc] initWithUpdater: self connection: connection];
+        coRreEncodingReader = [[CoRREEncodingReader alloc] initWithUpdater: self connection: connection];
+        hextileEncodingReader = [[HextileEncodingReader alloc] initWithUpdater: self connection: connection];
+        tightEncodingReader = [[TightEncodingReader alloc] initWithUpdater: self connection: connection];
+        zlibEncodingReader = [[ZlibEncodingReader alloc] initWithUpdater: self connection: connection];
+        zrleEncodingReader = [[ZRLEEncodingReader alloc] initWithUpdater: self connection: connection];
+        zlibHexEncodingReader = [[ZlibHexEncodingReader alloc] initWithUpdater: self connection: connection];
+
+        desktopNameReader = [[DesktopNameEncodingReader alloc] initWithUpdater:self
+                                                     connection:connection];
+        cursorReader = [[CursorPseudoEncodingReader alloc] initWithUpdater: self
+                connection: connection];
+
+        invalidRects = [[NSMutableArray alloc] init];
+        shouldResize = NO;
 	}
     return self;
 }
@@ -67,23 +74,32 @@
 - (void)dealloc
 {
     [headerReader release];
+    [rectHeaderReader release];
+
     [rawEncodingReader release];
     [copyRectangleEncodingReader release];
     [rreEncodingReader release];
     [coRreEncodingReader release];
     [hextileEncodingReader release];
     [tightEncodingReader release];
-    [rectHeaderReader release];
 	[zlibEncodingReader release];
 	[zrleEncodingReader release];
 	[zlibHexEncodingReader release];
+
+    [desktopNameReader release];
+    [cursorReader release];
+
+    [invalidRects release];
+
     [super dealloc];
 }
 
 - (void)setFrameBuffer:(id)aBuffer
 {
+    bytesPerPixel = [aBuffer bytesPerPixel];
     [rawEncodingReader setFrameBuffer:aBuffer];
     [copyRectangleEncodingReader setFrameBuffer:aBuffer];
+    [cursorReader setFrameBuffer:aBuffer];
     [rreEncodingReader setFrameBuffer:aBuffer];
     [coRreEncodingReader setFrameBuffer:aBuffer];
     [hextileEncodingReader setFrameBuffer:aBuffer];
@@ -93,9 +109,10 @@
 	[zlibHexEncodingReader setFrameBuffer:aBuffer];
 }
 
-- (void)resetReader
+- (void)readMessage
 {
-    [target setReader:headerReader];
+    [connection setReader:headerReader];
+    [connection frameBufferUpdateBeginning];
 }
 
 - (void)setHeader:(NSData*)header
@@ -107,14 +124,20 @@
 #endif
     memcpy(&msg.pad, [header bytes], sizeof(msg) - 1);
     numberOfRects = ntohs(msg.nRects);
-    [connection pauseDrawing];
-    [target setReader:rectHeaderReader];
+    if (numberOfRects > 0)
+        [connection setReader:rectHeaderReader];
+    else
+        /* OSXvnc/Vine Server version 3.11 will sometimes send FramebufferUpdate
+         * messages with no rectangles. */
+        [self updateComplete];
 }
 
+/* Received header for a rectangle as part of update. Dispatch to appropriate
+ * encoding reader. */
 - (void)setRect:(NSData*)rectInfo
 {
-    id theReader = nil;
-    CARD32 e;
+    EncodingReader  *theReader = nil;
+    CARD32          e;
     rfbFramebufferUpdateRectHeader* msg = (rfbFramebufferUpdateRectHeader*)[rectInfo bytes];
 
 #ifdef COLLECT_STATS
@@ -124,95 +147,112 @@
     currentRect.origin.y = ntohs(msg->r.y);
     currentRect.size.width = ntohs(msg->r.w);
     currentRect.size.height = ntohs(msg->r.h);
-    if ((currentRect.size.width == 0) && (currentRect.size.height == 0)) {
+    e = ntohl(msg->encoding);
+    if (currentRect.size.width == 0 && currentRect.size.height == 0
+            && e != rfbEncodingPointerPos) {
 		// this is a hack for compatibility with OSXvnc 1.0
 		[self updateComplete];
 		return;
     }
-    e = ntohl(msg->encoding);
     switch(e) {
         case rfbEncodingRaw:
-//			NSLog(@"Raw Encoding");
             theReader = rawEncodingReader;
             break;
         case rfbEncodingCopyRect:
-//			NSLog(@"CopyRect Encoding");
             theReader = copyRectangleEncodingReader;
             break;
         case rfbEncodingRRE:
-//			NSLog(@"RRE Encoding");
             theReader = rreEncodingReader;
             break;
         case rfbEncodingCoRRE:
-//			NSLog(@"CoRRE Encoding");
             theReader = coRreEncodingReader;
             break;
         case rfbEncodingHextile:
-//			NSLog(@"Hextile Encoding");
             theReader = hextileEncodingReader;
             break;
 		case rfbEncodingZlib:
-//			NSLog(@"Zlib Encoding");
 			theReader = zlibEncodingReader;
 			break;
         case rfbEncodingTight:
-//			NSLog(@"Tight Encoding");
             theReader = tightEncodingReader;
             break;
 		case rfbEncodingZlibHex:
-//			NSLog(@"ZlibHex Encoding");
 			theReader = zlibHexEncodingReader;
 			break;
 		case rfbEncodingZRLE:
-//			NSLog(@"ZRLE Encoding");
 			theReader = zrleEncodingReader;
 			break;
+
+        // pseudo-encodings
+        case rfbEncodingDesktopName:
+            theReader = desktopNameReader;
+            break;
+        case rfbEncodingRichCursor:
+            theReader = cursorReader;
+			break;
+        case rfbEncodingPointerPos:
+            [connection serverMovedMouseTo:currentRect.origin];
+            [self didRect:nil];
+            return;
+        case rfbEncodingLastRect:
+            [self updateComplete];
+            return;
+        case rfbEncodingDesktopSize:
+            resize = currentRect.size;
+            shouldResize = YES;
+            [self didRect: nil];
+            return;
     }
     if(theReader == nil) {
         [connection terminateConnection:[NSString stringWithFormat:
             @"Unknown rectangle encoding %d -> exiting", e]];
     } else {
         [theReader setRectangle:currentRect];
-        [target setReader:theReader];
+        [theReader readEncoding];
+        if (e <= rfbEncodingMax) { // not a pseudo-encoding
+            [invalidRects addObject: [NSValue valueWithRect:currentRect]];
+            bytesRepresented += currentRect.size.width * currentRect.size.height
+                                    * bytesPerPixel;
+            rectsTransferred++;
+        }
     }
 }
 
+/* Message from encoder that it has finished drawing its rectangle. Read the
+ * next rectangle */
 - (void)didRect:(EncodingReader*)aReader
 {
-    id rlist = [aReader rectangleList];
-
 #ifdef COLLECT_STATS
     bytesTransferred += [aReader bytesTransferred];
-    bytesRepresented += currentRect.size.width * currentRect.size.height * [[aReader frameBuffer] bytesPerPixel];
     rectsTransferred++;
 #endif
-    if(rlist) {
-        [connection drawRectList:rlist];
-    } else {
-        [connection drawRectFromBuffer:currentRect];
-    }
+
     numberOfRects--;
     if(numberOfRects) {
-        [target setReader:rectHeaderReader];
+        [connection setReader:rectHeaderReader];
     } else {
-		[self updateComplete];
+        [self updateComplete];
     }
 }
 
+#if 0
 - (double)compressRatio
 {
     return (bytesRepresented/bytesTransferred);
 }
+#endif
 
 - (double)rectanglesTransferred
 {
     return rectsTransferred;
 }
 
+#if 0
 - (double)bytesTransferred
 {
     return bytesTransferred;
 }
+#endif
 
 - (double)bytesRepresented
 {
@@ -221,8 +261,26 @@
 
 - (void)updateComplete
 {
-	[target performSelector:action withObject:self];
-	[connection flushDrawing];
+    if (shouldResize) {
+        shouldResize = NO;
+        [connection frameBufferUpdateCompleteWithResize:resize];
+    } else {
+        int     i;
+
+        /* We only mark the changed rectangles as dirty at the end of the
+         * update.  This means that normally the framebuffer will not be drawn,
+         * and thus the user will not see partial updates. However, if the user
+         * scrolls, the OS will ask the RFBView to draw, and it will draw from
+         * the possibly partially updated framebuffer. */
+        for (i = 0; i < [invalidRects count]; i++) {
+            [connection invalidateRect: [[invalidRects objectAtIndex:i]
+                rectValue]];
+        }
+        [invalidRects removeAllObjects];
+
+        [connection frameBufferUpdateComplete];
+    }
+    [protocol messageReaderDone];
 }
 
 @end

b22e05843ba6348af900cfbbc23d4efe261972dc
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2010-10-23 18:30:15 +0000

    Removed several options from doYouSupport:, because we can test for method definition.
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@354 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

--- Source/ServerStandAlone.m
+++ Source/ServerStandAlone.m
@@ -40,11 +40,11 @@
 		case EDIT_ADDRESS:
 		case EDIT_PORT:
 		case EDIT_NAME:
-		case DELETE:
-		case SERVER_SAVE:
+		//case DELETE:
+		//case SERVER_SAVE:
 		case EDIT_PASSWORD:
 		case CONNECT:
-		case ADD_SERVER_ON_CONNECT:
+		//case ADD_SERVER_ON_CONNECT:
 			return YES;
 		case SAVE_PASSWORD:
 			return mAddToServerListOnConnect;

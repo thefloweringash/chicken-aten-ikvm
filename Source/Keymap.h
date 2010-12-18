71909e87643e76852bdca9803e34a0d761b56e64
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2010-12-18 21:19:26 +0000

    Moved extern's for Keymap.m into Keymap.h.
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@499 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

---
+++ Source/Keymap.h
@@ -0,0 +1,29 @@
+/* Keymap.m
+ * Copyright (C) 2010 Dustin Cartwright
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+extern const unsigned int page0[];
+extern const unsigned int page1[];
+extern const unsigned int page3[];
+extern const unsigned int page4[];
+extern const unsigned int page5[];
+extern const unsigned int page6[];
+extern const unsigned int pagee[];
+extern const unsigned int page30[];
+extern const unsigned int pagef6[];
+extern const unsigned int pagef7[];

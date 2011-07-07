0377fcf718abf9c63c37ddff61e6576088413c56
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2011-07-07 15:20:05 +0000

    Make sure stderr gets closed in helper script so that main program can detect EOF ssh program.
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@690 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

--- Resources/ssh-helper.sh
+++ Resources/ssh-helper.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo Chicken ssh-helper: $* >&2 &&
-/bin/cat $CHICKEN_NAMED
+# If ssh dies unexpectedly, we want Chicken to see standard error get closed, so
+# we prevent either the shell or /bin/at from keeping it open.
+exec /bin/cat $CHICKEN_NAMED 2>/dev/null

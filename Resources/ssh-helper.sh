d43d43f3d069fb62c895e62c3ba86affec0a7ebd
dustinac <dustinac@9ce1af06-8ae8-4ba5-be20-3b57e42882c5>
2011-10-22 22:21:29 +0000

    Fixed typo in comment.
    git-svn-id: svn://svn.code.sf.net/p/chicken/code/trunk/cotvnc@770 9ce1af06-8ae8-4ba5-be20-3b57e42882c5

--- Resources/ssh-helper.sh
+++ Resources/ssh-helper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Chicken ssh-helper: $* >&2 &&
 # If ssh dies unexpectedly, we want Chicken to see standard error get closed, so
-# we prevent either the shell or /bin/at from keeping it open.
+# we prevent either the shell or /bin/cat from keeping it open.
 exec /bin/cat $CHICKEN_NAMED 2>/dev/null
